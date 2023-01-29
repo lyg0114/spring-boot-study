3f0c02c3c9a3da42606558c8215186946deb9352
lyg0114 <whdnseowkd@naver.com>
2023-01-29 12:53:31 +0900

    refactoring

---
+++ src/main/java/yglee/springbootstudy/HelloController.java
@@ -0,0 +1,7 @@
+package yglee.springbootstudy;
+
+public class HelloController {
+  public String hello(String name){
+    return "Hello " + name;
+  }
+}
--- src/main/java/yglee/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/yglee/springbootstudy/SpringBootStudyApplication.java
@@ -12,21 +12,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class SpringBootStudyApplication {
-
   public static void main(String[] args) {
     TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
+      HelloController helloController = new HelloController();
       servletContext.addServlet("frontcontroller", new HttpServlet() {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
           if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
             String name = req.getParameter("name");
+            String ret = helloController.hello(name);
             resp.setStatus(HttpStatus.OK.value());
             resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().println("Hello Servlet" + name);
+            resp.getWriter().println(ret);
           } else if (req.getRequestURI().equals("/user")) {
-
           } else {
             resp.setStatus(HttpStatus.NOT_FOUND.value());
           }
